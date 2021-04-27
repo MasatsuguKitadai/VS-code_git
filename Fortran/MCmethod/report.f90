2a7a272f1f0d54fa2fd4d1b6a1c389ad8158dd4d
Masatsugu <b8123026@gmail.com>
2021-04-27 12:20:50 +0900

    name

--- Fortran/MCmethod/report.f90
+++ Fortran/MCmethod/report.f90
@@ -6,10 +6,11 @@ program main
     integer, parameter :: num = 10000
     integer, parameter :: case = 5
     integer i, j, n
-    real x, y, a, b, pi
+    real x, y, a, b, pi, f_1, f_2, f_3, f_4, f_5
     real f(case),s(case)
     real r(num + 1)
-    real area(num + 1,case)
+    real area(num + 1, case)
+    real dif(num + 1, case)
 
     !** create random number *********************
     
@@ -34,7 +35,7 @@ program main
        s(j) = 0
     10 continue
 
-    open(17, file = 'output/result.dat', status='replace')
+    open(17, file = 'result/result.dat', status='replace')
 
     do 20 i = 1, num
         x = r(i)
@@ -61,5 +62,26 @@ do 30 i = 1, num
 
 close(17)
 
+open(18, file= 'result/difference.dat', status='replace')
+
+f_1 = 1.0/2.0
+f_2 = 1.0/3.0
+f_3 = 2.0/pi
+f_4 = 2.0/pi
+f_5 = 2.0
+
+do 40 i = 1, num
+    dif(i,1) = area(i,1) - f_1
+    dif(i,2) = area(i,2) - f_2
+    dif(i,3) = area(i,3) - f_3
+    dif(i,4) = area(i,4) - f_4
+    dif(i,5) = area(i,5) - f_5
+40 continue
+
+do 50 i = 1, num
+    write (18,*) i,dif(i,1),dif(i,2),dif(i,3),dif(i,4),dif(i,5)
+50 continue
+
+close(18)
 
 end program
