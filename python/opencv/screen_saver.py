5f96e2e4e94a1c2d3c0899f34900b3e8a62d318b
Masatsugu <b8123026@gmail.com>
2021-05-28 21:13:12 +0900

    saver

--- python/opencv/screen_saver.py
+++ python/opencv/screen_saver.py
@@ -7,13 +7,13 @@ import pyautogui
 # movie = cv2.VideoCapture('C:/Users/Kitadai/Videos/test.mp4')
 
 # droidcam
-# movie = cv2.VideoCapture('http://192.168.0.3:4747/video')
+movie = cv2.VideoCapture('http://192.168.0.3:4747/video')
 
 # iriunwebcam for ubuntu
 # movie = cv2.VideoCapture(1)
 
 # iriunwebcam for windows
-movie = cv2.VideoCapture(0)
+# movie = cv2.VideoCapture(0)
 
 before = None  # 前回の画像を保存する変数
 fps = int(movie.get(cv2.CAP_PROP_FPS))  # 動画のFPSを取得
@@ -52,21 +52,21 @@ while True:
     # 輪郭のデータを得る
     contours = cv2.findContours(
         thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
- 
+
     # ウィンドウで表示
     # cv2.imshow('target_frame', gray)
     # cv2.imshow('target_frame', frameDelta)
     # cv2.imshow('target_frame', thresh)
     # cv2.imshow('target_frame', contours)
 
-    pixel_number = np.size(contours) #全ピクセル数をpixel_numberに代入
-    pixel_sum = np.sum(contours) #全ピクセルの輝度の合計をpixel_sumに代入
-    white_pixel_number = pixel_sum/255 #白いピクセルの数を計算しwhite_pixel_numberに代入
-    print(white_pixel_number) #白いピクセルの数を表示
+    pixel_number = np.size(contours)  # 全ピクセル数をpixel_numberに代入
+    pixel_sum = np.sum(contours)  # 全ピクセルの輝度の合計をpixel_sumに代入
+    white_pixel_number = pixel_sum/255  # 白いピクセルの数を計算しwhite_pixel_numberに代入
+    print(white_pixel_number)  # 白いピクセルの数を表示
 
     # 白いピクセルの数が一定値を超えたら画面を変更 or Enterキーが押されたらループを抜ける
     if white_pixel_number > 9000:
-        pyautogui.hotkey('winleft','winright','d')
+        pyautogui.hotkey('winleft', 'winright', 'd')
         break
 
     elif cv2.waitKey(1) == 13:
