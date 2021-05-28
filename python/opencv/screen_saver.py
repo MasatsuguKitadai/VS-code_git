65bb7de002e38259ba4fe21da23f359d5fcc4e6a
MasatsuguKitadai <b8123026@gmail.com>
2021-05-28 11:29:50 +0900

    screensaver

--- python/opencv/screen_saver.py
+++ python/opencv/screen_saver.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+import numpy as np
+import pyautogui
 
 # lab_windows
 # movie = cv2.VideoCapture('C:/Users/Kitadai/Videos/test.mp4')
@@ -7,12 +9,23 @@ import time
 # droidcam
 # movie = cv2.VideoCapture('http://192.168.0.3:4747/video')
 
-# iriun
-movie = cv2.VideoCapture(1)
+# iriunwebcam for ubuntu
+# movie = cv2.VideoCapture(1)
 
-red = (0, 0, 255)  # 枠線の色
+# iriunwebcam for windows
+movie = cv2.VideoCapture(0)
+
 before = None  # 前回の画像を保存する変数
 fps = int(movie.get(cv2.CAP_PROP_FPS))  # 動画のFPSを取得
+
+# ウィンドウでの再生速度を元動画と合わせる
+time.sleep(1/fps)
+
+# 画像を取得
+ret, frame = movie.read()
+
+# 画像の取得が安定するように2秒間停止
+# time.sleep(2)
 
 while True:
     # 画像を取得
@@ -28,7 +41,7 @@ while True:
         continue
 
     # 現在のフレームと移動平均との差を計算
-    cv2.accumulateWeighted(gray, before, 0.96)
+    cv2.accumulateWeighted(gray, before, 0.95)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(before))
 
     # frameDeltaの画像を２値化
@@ -39,26 +52,24 @@ while True:
     # 輪郭のデータを得る
     contours = cv2.findContours(
         thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-
-    # ウィンドウでの再生速度を元動画と合わせる
-    time.sleep(1/fps)
-
-    # 差分があった点を画面に描く
-    for target in contours:
-        x, y, w, h = cv2.boundingRect(target)
-        if w < 30:
-            continue  # 小さな変更点は無視
-        frame = cv2.rectangle(frame, (x, y), (x+w, y+h), red, 2)
  
     # ウィンドウで表示
     # cv2.imshow('target_frame', gray)
     # cv2.imshow('target_frame', frameDelta)
     # cv2.imshow('target_frame', thresh)
     # cv2.imshow('target_frame', contours)
-    cv2.imshow('frame', frame)
 
-    # Enterキーが押されたらループを抜ける
-    if cv2.waitKey(1) == 13:
+    pixel_number = np.size(contours) #全ピクセル数をpixel_numberに代入
+    pixel_sum = np.sum(contours) #全ピクセルの輝度の合計をpixel_sumに代入
+    white_pixel_number = pixel_sum/255 #白いピクセルの数を計算しwhite_pixel_numberに代入
+    print(white_pixel_number) #白いピクセルの数を表示
+
+    # 白いピクセルの数が一定値を超えたら画面を変更 or Enterキーが押されたらループを抜ける
+    if white_pixel_number > 9000:
+        pyautogui.hotkey('winleft','winright','d')
+        break
+
+    elif cv2.waitKey(1) == 13:
         break
 
 cv2.destroyAllWindows()  # ウィンドウを破棄
